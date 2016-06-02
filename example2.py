94c07f61bab39da67fac62df06eabded70549675
Nate Ricklin <nate@rickl.in>
2016-06-02 15:38:27 -0600

    indentation error

--- example2.py
+++ example2.py
@@ -2,7 +2,7 @@ from gbdxtools import Interface
 gbdx = Interface()
 
 task = gbdx.Task('ricklin-gdal-test')
-task.inputs.data = ' s3://gbd-customer-data/7b216bd9-6523-4ca9-aa3b-1d8a5994f054/sethtest/'
+task.inputs.data = 's3://gbd-customer-data/7b216bd9-6523-4ca9-aa3b-1d8a5994f054/sethtest/'
 #task.inputs.filetype = 'tif'
 task.inputs.execution_strategy = 'runonce'
 
@@ -15,6 +15,6 @@ gdal_translate  $indir/out.vrt  $outdir/out.tif
 task.inputs.command = command
 
 
-workflow = gbdx.Workflow([task, task2])
+workflow = gbdx.Workflow([task])
 workflow.savedata(task.outputs.data, location='seth_test_output')
 workflow.execute()
--- task.py
+++ task.py
@@ -21,28 +21,28 @@ if filetype or execution_strategy=='foreach':
 	files.extend( glob2.iglob(indir + "/**/*." + filetype.upper()) )
 
 	for abs_filename in files:
-	    relative_filename = abs_filename.replace(indir,'').strip('/')
-	    relative_path = '/'.join(relative_filename.split('/')[:-1])
-	    filename = relative_filename.split('/')[-1:][0]
-	    print relative_filename
-	    print relative_path
-	    print filename
-	    output_directory = outdir + '/' + relative_path
-	    output_abs_filename = output_directory + '/' + filename
-	    try:
-	        os.makedirs(output_directory)
-	    except:
-	    	pass
-	    command = cmd.replace('$input',abs_filename)
-	    command = command.replace('$output',output_abs_filename)
-	    command = cmd.replace('$indir',indir)
+		relative_filename = abs_filename.replace(indir,'').strip('/')
+		relative_path = '/'.join(relative_filename.split('/')[:-1])
+		filename = relative_filename.split('/')[-1:][0]
+		print relative_filename
+		print relative_path
+		print filename
+		output_directory = outdir + '/' + relative_path
+		output_abs_filename = output_directory + '/' + filename
+		try:
+			os.makedirs(output_directory)
+		except:
+			pass
+		command = cmd.replace('$input',abs_filename)
+		command = command.replace('$output',output_abs_filename)
+		command = cmd.replace('$indir',indir)
 		command = cmd.replace('$outdir',outdir)
-	    proc = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	    out, err = proc.communicate()
-	    status = proc.returncode
-	    print out
+		proc = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		out, err = proc.communicate()
+		status = proc.returncode
+		print out
 
 
 status = { "status": "success", "reason": "Ran stuff." }
 with open('/mnt/work/status.json', 'w') as outfile:
-    json.dump(status, outfile)
+	json.dump(status, outfile)
